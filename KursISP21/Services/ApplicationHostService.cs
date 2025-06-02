16491fdf793514f6744927af4547831cba2ac3b8
rakdav <rakdav@inbox.ru>
2025-06-02 14:57:03 +0200

    2/06

---
+++ KursISP21/Services/ApplicationHostService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Wpf.Ui;
+
+namespace KursISP21.Services
+{
+    public class ApplicationHostService(IServiceProvider serviceProvider) : IHostedService
+    {
+        private INavigationWindow? _navigationWindow;
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await HandleActionAsync();
+        }
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await Task.CompletedTask;
+        }
+        private async Task HandleActionAsync()
+        {
+            await Task.CompletedTask;
+            if (!Application.Current.Windows.OfType<MainWindow>().Any())
+            {
+                _navigationWindow = (serviceProvider.GetService(typeof(INavigationWindow)) as INavigationWindow)!;
+                _navigationWindow!.ShowWindow();
+                _ = _navigationWindow.Navigate(typeof(Views.Pages.StartPage));
+            }
+            await Task.CompletedTask;
+        }
+    }
+}
