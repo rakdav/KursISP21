9573f66cf47f15e04b080e03ec285f64d106c340
rakdav <rakdav@inbox.ru>
2025-06-05 14:59:31 +0200

    5/06-last

--- KursISP21/ViewModels/AuthorViewModel.cs
+++ KursISP21/ViewModels/AuthorViewModel.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using KursISP21.Helpers;
+using KursISP21.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +12,50 @@ namespace KursISP21.ViewModels
 {
     public class AuthorViewModel:BaseViewModel
     {
+        private KursovayaContext db = new KursovayaContext();
+        private RelayCommand? addCommand;
+        private RelayCommand? editCommand;
+        private RelayCommand? deleteCommand;
+        public ObservableCollection<Author> Authors { get; set; }
 
+        public AuthorViewModel()
+        {
+            db.Database.EnsureCreated();
+            db.Authors.Load();
+            Authors = db.Authors.Local.ToObservableCollection();
+        }
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                  (addCommand = new RelayCommand((o) =>
+                  {
+
+                  }));
+            }
+        }
+        public RelayCommand EditCommand
+        {
+            get
+            {
+                return editCommand ??
+                  (editCommand = new RelayCommand((o) =>
+                  {
+
+                  }));
+            }
+        }
+        public RelayCommand DeleteCommand
+        {
+            get
+            {
+                return deleteCommand ??
+                  (deleteCommand = new RelayCommand((o) =>
+                  {
+
+                  }));
+            }
+        }
     }
 }
